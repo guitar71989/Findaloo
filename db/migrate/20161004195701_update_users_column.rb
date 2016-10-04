2451a094b333d369c24e631bb8c395077001a96c
Sam Cross <samuel.harrison.cross@gmail.com>
2016-10-04 18:16:45 -0400

    Update user validations

--- db/migrate/20161004195701_update_users_column.rb
+++ db/migrate/20161004195701_update_users_column.rb
@@ -1,5 +1,7 @@
 class UpdateUsersColumn < ActiveRecord::Migration
   def change
-    remove_column :users :username
+    remove_column :users, :username
+    add_column :users, :username, :string, null: false
+    add_index :users, :username, unique: true
   end
 end
